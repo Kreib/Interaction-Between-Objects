d1827d3574c0b44cdf7764d4d814678fd21ea1c4
Kreib <Kreib@users.noreply.github.com>
2018-08-24 18:43:08 -0500

    Resolvi Ejercicio de Color

---
+++ unity-sessions-interaction-between-objects-setup/interaction-between-gameobjects/Assets/Scripts/CubeChangesPlayerLife/GameMaster.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CubeChangesPlayerLife
+{
+	public class GameMaster : MonoBehaviour {
+        // Use this for initialization
+        public int Life = 10;
+        public GameObject Sphere;
+        void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+            if (Life <= 0)
+            {
+                Sphere.SetActive(false);
+            }
+		}
+         void OnMouseDown()
+        {
+            Life -= 1;
+        }
+        private void OnGUI()
+        {
+            if (Life >= 0)
+            {
+                GUILayout.Label("Player Life " + Life);
+            }
+            else
+            {
+                GUILayout.Label("YOU DIED");
+            }
+               
+        }
+    }
+}
